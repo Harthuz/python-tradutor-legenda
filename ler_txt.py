b4d76e0e9fe565e49754aa41d971070ea37ae0e0
Hernandes Arthur <hernandeshass910@gmail.com>
2024-12-15 16:56:39 -0300

    feat: adiciona importação de modulos

--- ler_txt.py
+++ ler_txt.py
@@ -1,3 +1,9 @@
+diretório_legenda = "Living Single - S01 E03 - Whose Date Is It Anyway (480p - HULU Web-DL).srt"
+nome_final = "traduzido.srt"
+
+from module import module
+module()
+
 from deep_translator import GoogleTranslator
 from tqdm import tqdm  # Importar tqdm para a barra de progresso
 
@@ -22,7 +28,7 @@ def traduzir_bloco(blocos):
     return blocos
 
 # Ler o arquivo de legendas .srt
-with open("Living Single - S01 E01 - Judging by the Cover (480p - HULU Web-DL).srt", 'r', encoding='utf-8-sig') as f:
+with open(diretório_legenda, 'r', encoding='utf-8-sig') as f:
     texto = f.readlines()
 
 # Lista para armazenar os blocos de legendas
@@ -62,7 +68,7 @@ for i in tqdm(range(0, len(bloco_todos), 20), desc="Traduzindo Blocos", unit="bl
     bloco_traduzido_todos.extend(blocos_traduzidos)
 
 # Salvar os blocos traduzidos em um novo arquivo .srt
-with open("legenda_traduzida.srt", 'w', encoding='utf-8-sig') as f:
+with open(nome_final, 'w', encoding='utf-8-sig') as f:
     for bloco in bloco_traduzido_todos:
         f.write(f"{bloco[0]}\n")
         f.write(f"{bloco[1]}\n")
---
+++ module.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+
+def instalar_pacote(pacote):
+    try:
+        __import__(pacote)  # Tenta importar o pacote
+    except ImportError:
+        # Se o pacote não estiver instalado, instala
+        subprocess.check_call([sys.executable, "-m", "pip", "install", pacote])
+
+# Lista de pacotes necessários
+pacotes_necessarios = ['deep-translator', 'tqdm']  # Exemplo de pacotes
+
+def module():
+    # Instala os pacotes necessários
+    for pacote in pacotes_necessarios:
+        instalar_pacote(pacote)
+
+# Resto do seu código
+print("Pacotes instalados, agora o script pode ser executado normalmente.")
